031401e71b4aaecffbc0090ae180eca4662c6eea
nicktecno <nick.tecno@gmail.com>
2021-05-18 10:45:58 -0300

    feat: Prepare Project to deploy B

--- src/components/FileList/FileList.js
+++ src/components/FileList/FileList.js
@@ -1,7 +1,7 @@
 import { Badge } from "@material-ui/core";
 import { img_300, unavailable } from "../../config/config";
 import TransitionsModal from "../ContentModal/ContentModal";
-import { ContainerMedia, Poster, Subtitle, Title } from "./FileListStyled";
+import { Poster, Subtitle, Title } from "./FileListStyled";
 
 const FileList = ({ id, poster, title, date, media_type, vote_average }) => {
   return (
